85b5bbd9b0db139a7139059fd815030ddbf3e9e1
rubinkim <kimyongha2011@gmail.com>
2023-12-17 15:56:42 +0900

    caresense2_fullset.ipynb

--- coupang_bd.py
+++ coupang_bd.py
@@ -1,7 +1,9 @@
-ï»¿host = "brd.superproxy.io:22225"
+#-*- coding: utf-8 -*-
+
+host = "brd.superproxy.io:22225"
 user_name = "brd-customer-hl_878d4fe7-zone-unblocker"
 password = "ma5cz45h7cnv"
 
-# À§ÀÇ Á¤º¸µéÀ» ÀÌ¿ëÇØ¼­ proxy_urlÀ» ¸¸µç´Ù.
+# ìœ„ì˜ ì •ë³´ë“¤ì„ ì´ìš©í•´ì„œ proxy_urlì„ ë§Œë“ ë‹¤.
 proxy_url = f"https://{user_name}:{password}@{host}"
 print(proxy_url)
