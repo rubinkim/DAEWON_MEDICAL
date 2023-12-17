b3fd18aabf00a2eec603fb24973d404d1903918c
rubinkim <kimyongha2011@gmail.com>
2023-12-17 15:31:15 +0900

    caresense2_fullset.ipynb

--- coupang_bd.py
+++ coupang_bd.py
@@ -1,7 +1,7 @@
-host = "brd.superproxy.io:22225"
+癤퓀ost = "brd.superproxy.io:22225"
 user_name = "brd-customer-hl_878d4fe7-zone-unblocker"
 password = "ma5cz45h7cnv"
 
-# proxy_url을 만든다
+# 위의 정보들을 이용해서 proxy_url을 만든다.
 proxy_url = f"https://{user_name}:{password}@{host}"
 print(proxy_url)
