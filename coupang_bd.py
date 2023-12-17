0fe1cff2e1b0dc11910cbe7631d83af5292b8c4d
rubinkim <kimyongha2011@gmail.com>
2023-12-17 15:21:48 +0900

    caresense2_fullset.ipynb

--- coupang_bd.py
+++ coupang_bd.py
@@ -1,3 +1,7 @@
 host = "brd.superproxy.io:22225"
 user_name = "brd-customer-hl_878d4fe7-zone-unblocker"
 password = "ma5cz45h7cnv"
+
+# proxy_url을 만든다
+proxy_url = f"https://{user_name}:{password}@{host}"
+print(proxy_url)
