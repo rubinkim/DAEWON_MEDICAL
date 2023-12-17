976cc13a19472d9b88c2a5da577da7dc71f6ef92
rubinkim <kimyongha2011@gmail.com>
2023-12-17 16:08:44 +0900

    caresense2_fullset.ipynb

--- coupang_bd.py
+++ coupang_bd.py
@@ -15,3 +15,5 @@ proxies = {"http": proxy_url, "https": proxy_url}
 keyword = input("검색할 제품 입력 : ")
 url = f"https://www.coupang.com/np/search?component=&q={keyword}"
 response = requests.get(url, proxies=proxies, verify=False)     # verify = False를 넣어주면 ssl인증과정을 생략하기 때문에 에러가 발생하지 않는다.
+html = response.text
+print(html[:1000])
