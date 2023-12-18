45419539e9dbe4cc3108f6f6822996df8500a733
rubinkim <kimyongha2011@gmail.com>
2023-12-18 15:42:54 +0900

    more on coupang_bd.py

--- coupang_bd.py
+++ coupang_bd.py
@@ -63,3 +63,4 @@ recent&backgroundColor=&searchProductCount=1215796&component=&rating=0&sorter=sc
 # 이중 한꺼번에 72개씩 보기와 관련된 부분은 맨마지막에 &listSize=72로 표기되어 있다. 이부분만 복붙하자. 페이지를 3으로 해보자.
 # https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&page=3&listSize=72
 # 페이지는 3이고 72개씩 보기로 잘 변경되어 있는게 확인된다.
+# 이제 우리는 이걸로 위의 라인 18에 있는 url을 변경해준다.
